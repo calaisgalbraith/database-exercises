c1440cc9b2e67b3bad8bd6c525aad22e5102d05e
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-23 10:38:36 -0500

    finished delete_exercises:

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,12 +1,24 @@
 USE codeup_test_db;
-
---Write SELECT statements for:
-    -- Albums released after 1991
-    -- Albums with the genre 'disco'
-    -- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
-
--- Make sure to put appropriate captions before each SELECT.
-
+-- Write SELECT statements
 -- Convert the SELECT statements to DELETE.
 
--- Use the MySQL command line client to make sure your records really were removed.
+-- SELECT Albums released after 1991
+    SELECT 'Albums released after 1991:' as ' ';
+    SELECT * FROM albums WHERE release_date > 1991;
+
+    SELECT 'Albums released after 1991:' as ' ';
+    DELETE FROM albums WHERE release_date > 1991;
+
+-- SELECT Albums with the genre 'disco'
+    SELECT 'Albums with the genre ''disco'':' as ' ';
+    SELECT * FROM albums WHERE genre = 'disco';
+
+    SELECT 'Albums with the genre ''disco'':' as ' ';
+    DELETE FROM albums WHERE genre = 'disco';
+
+-- -- SELECT Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+    SELECT 'Albums by ''The Beatles'':' as ' ';
+    SELECT * FROM albums WHERE artist = 'The Beatles';
+
+    SELECT 'Albums by ''The Beatles'':' as ' ';
+    DELETE FROM albums WHERE artist = 'The Beatles';
