c073d7008e4c553ca635c4971ef02171e90a8687
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-22 23:24:15 -0500

    created file

---
+++ delete_exercises.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+--Write SELECT statements for:
+    -- Albums released after 1991
+    -- Albums with the genre 'disco'
+    -- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+
+-- Make sure to put appropriate captions before each SELECT.
+
+-- Convert the SELECT statements to DELETE.
+
+-- Use the MySQL command line client to make sure your records really were removed.
