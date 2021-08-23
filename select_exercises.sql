f52e0d76cec4f383e79b9eeb44f4374edfac1ba4
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-23 09:50:32 -0500

    finished exercises TODO: get rid of first placeholder when running

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,23 +1,23 @@
---Remember to USE the codeup_test_db.
+-- Remember to USE the codeup_test_db.
 USE codeup_test_db;
 
 -- Write queries to find the following information. Before each item, output a caption explaining the results:
 
-PRINT 'The name of all albums by Pink Floyd: ';
+SELECT 'The name of all albums by Pink Floyd:' as ' ';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
-PRINT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released: ';
-SELECT release_date FROM albums WHERE name = ' Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released:' as ' ';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-PRINT 'The genre for Nevermind: ';
+SELECT 'The genre for Nevermind:' as ' ';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
-PRINT 'Albums that were released in the 1990s: ';
+SELECT 'Albums that were released in the 1990s:' as ' ';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-PRINT 'Albums that had less than 20 million certified sales: ';
+SELECT 'Albums that had less than 20 million certified sales:' as ' ';
 SELECT name FROM albums WHERE sales < 20.0;
 
---Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
-PRINT 'All the albums with a genre of "Rock": ';
+-- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT 'All the albums with a genre of "Rock":' as ' ';
 SELECT name FROM albums WHERE genre = 'rock';
