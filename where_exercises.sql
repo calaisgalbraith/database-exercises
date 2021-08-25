017913c4ea2e6b82e87d22203d857467a05076d6
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-25 10:15:37 -0500

    added e. format

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,49 +3,49 @@ USE employees;
 
 -- DONE: Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)
     SELECT *
-    from employees
-    where first_name in ('Irena', 'Vidya', 'Maya');
+    from employees e
+    where e.first_name in ('Irena', 'Vidya', 'Maya');
 
 
 -- DONE: Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
 -- DONE: Add a condition to find everybody with those names who is also male — 441 rows.
     SELECT *
-    from employees
-    where (first_name = 'Irena'
-       OR  first_name = 'Vidya'
-       OR first_name =  'Maya')
-       AND gender = 'M';
+    from employees e
+    where (e.first_name = 'Irena'
+       OR  e.first_name = 'Vidya'
+       OR e.first_name =  'Maya')
+       AND e.gender = 'M';
 
 
 -- DONE: Find all employees whose last name starts with 'E' — 7,330 rows.
     SELECT *
-    from employees
-    where last_name LIKE "E%";
+    from employees.employees e
+    where e.last_name LIKE "E%";
 
 
 -- DONE: Find all employees with a 'q' in their last name — 1,873 rows.
     SELECT *
-    from employees
-    where last_name LIKE "%q%";
+    from employees.employees e
+    where e.last_name LIKE "%q%";
 
 
 -- DONE: Find all employees whose last name starts or ends with 'E' — 30,723 rows.
     SELECT *
-    from employees
-    where last_name LIKE "E%"
-        OR last_name LIKE "%e";
+    from employees.employees e
+    where e.last_name LIKE "E%"
+        OR e.last_name LIKE "%e";
 
 
 -- DONE: Find all employees whose last name starts and ends with 'E' — 899 rows.
     SELECT *
     from employees
-    where last_name LIKE "E%"
-        AND last_name LIKE "%e";
+    where e.last_name LIKE "E%"
+        AND e.last_name LIKE "%e";
 
 
 --  Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
     SELECT *
     from employees
-    where last_name LIKE "%q%"
-      AND last_name NOT LIKE "%qu%";
+    where e.last_name LIKE "%q%"
+      AND e.last_name NOT LIKE "%qu%";
 
