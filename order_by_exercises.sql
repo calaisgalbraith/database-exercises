4a04588fc57415d6e647b54ddf949f7f7a604e54
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-25 10:34:11 -0500

    added .e format

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,39 +2,39 @@ USE employees;
 
 -- Modify your first query to order by first name.
     SELECT *
-    from employees
-    where first_name in ('Irena', 'Vidya', 'Maya')
-    ORDER BY first_name asc;
+    from employees.employees e
+    where e.first_name in ('Irena', 'Vidya', 'Maya')
+    ORDER BY e.first_name asc;
 
 -- Update the query to order by first name and then last name.
     SELECT *
-    from employees
-    where first_name in ('Irena', 'Vidya', 'Maya')
-    ORDER BY first_name asc, last_name asc;
+    from employees.employees e
+    where e.first_name in ('Irena', 'Vidya', 'Maya')
+    ORDER BY e.first_name asc, e.last_name asc;
 
 -- Change the ORDER BY clause so that you order by last name before first name.
     SELECT *
-    from employees
-    where first_name in ('Irena', 'Vidya', 'Maya')
-    ORDER BY last_name asc, first_name asc;
+    from employees.employees e
+    where e.first_name in ('Irena', 'Vidya', 'Maya')
+    ORDER BY e.last_name asc, e.first_name asc;
 
 -- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
     SELECT *
-    from employees
-    where last_name LIKE "E%"
-      OR last_name LIKE "%e"
-      OR last_name LIKE "%e%"
-      ORDER BY emp_no;
+    from employees.employees e
+    where e.last_name LIKE "E%"
+      OR e.last_name LIKE "%e"
+      OR e.last_name LIKE "%e%"
+      ORDER BY e.emp_no;
 
 -- Now reverse the sort order for both queries and compare results.
     SELECT *
-    from employees
-    where first_name in ('Irena', 'Vidya', 'Maya')
-    ORDER BY last_name desc, first_name desc;
+    from e.employees.employees e
+    where e.first_name in ('Irena', 'Vidya', 'Maya')
+    ORDER BY e.last_name desc, first_name desc;
 
     SELECT *
-    from employees
-    where last_name LIKE "E%"
-    OR last_name LIKE "%e"
-    OR last_name LIKE "%e%"
-    ORDER BY emp_no desc;
+    from employees.employees e
+    where e.last_name LIKE "E%"
+    OR e.last_name LIKE "%e"
+    OR e.last_name LIKE "%e%"
+    ORDER BY e.emp_no desc;
