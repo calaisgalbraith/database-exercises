a7a3845903ab970eb1b57bf941cc863a2f3a18d4
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-26 15:51:16 -0500

    finished bonus -s/o teaching staff

--- join_exercises.sql
+++ join_exercises.sql
@@ -21,7 +21,7 @@ WHERE e.gender like "F" AND dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 # DONE: Find the current titles of employees currently working in the Customer Service department.
-SELECT t.title as 'Title', COUNT(*) as 'Total'
+SELECT t.title as 'Title'
 FROM employees as e
          JOIN titles as t
               ON t.emp_no = e.emp_no
@@ -44,16 +44,18 @@ FROM employees as e
 WHERE dm.to_date = '9999-01-01'and s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
-# TODO Bonus Find the names of all current employees, their department name, and their current manager's name.
-SELECT DISTINCT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name as 'Department', CONCAT(me.first_name, ' ', me.last_name) as 'Manager'
+# DONE Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name as 'Department', CONCAT(ex.first_name, ' ', ex.last_name) AS 'Manager'
 FROM employees as e
     JOIN dept_emp de
         on e.emp_no = de.emp_no
     JOIN departments d
         on de.dept_no = d.dept_no
     JOIN dept_manager dm
-        on d.dept_no = dm.dept_no
-    JOIN employees me
-        on dm.emp_no = me.emp_no
-WHERE de.to_date = '9999-01-01' and ('9999-01-01' BETWEEN dm.from_date and dm.to_date);
+         on d.dept_no = dm.dept_no AND dm.to_date = '9999-01-01'
+    JOIN employees ex
+         on dm.emp_no = ex.emp_no
+WHERE de.to_date = '9999-01-01';
+
+
 
